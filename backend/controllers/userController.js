464faa7cc8112fce62d38689944647bd2d92a05b
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-17 20:02:17 +0530

    created the user registration now user can signup have not connected it to the frontend yet

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,13 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+//creating the token 
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 //route for user login
 
@@ -13,10 +22,42 @@ const registerUser = async(req, res) =>{
     try {
         
         const {name, email, password} = req.body;
+
+        // cheking if user already exist
+
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success:false, message: "User already exist"});
+        }
+        if(!validator.isEmail(email)){
+            return res.json({success:false, message: "Please enter a valid email"});
+        }
+
+        if(password.length < 8){
+            return res.json({success:false, message: "Please enter a strong password"});
+        }
+        
+        //hashing user password 
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        //returning the token 
+        const token = createToken(user._id);
+        res.json({success: true, token})
         
 
     } catch (error) {
-        
+        console.log(error);
+        res.json({success: false, message: error.message});
     }
 }
 
