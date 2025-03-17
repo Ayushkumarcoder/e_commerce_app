5dad65b6b18386e1cf8fe6d7b98d10ca62d7595f
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-17 23:22:20 +0530

    completed the addProduct section in productController and connected to the db
    now we are able to add products in db by adding the images to cloudinary
    and generating the url for images and storing images into the db

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
+import {v2 as cloudinary} from 'cloudinary';
 
-
-
+import productModel from '../models/productModel.js';
 
 // function to add product
 
@@ -14,8 +14,35 @@ const addProduct = async (req, res) => {
         const image3 =req.files.image3 && req.files.image3[0];
         const image4 =req.files.image4 && req.files.image4[0];
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1, image2, image3, image4);
+        const images = [image1, image2, image3, image4].filter((item)=> item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async(item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type:'image'});
+                return result.secure_url;
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            image: imagesUrl,
+            category,
+            subCategory,
+            sizes: JSON.parse(sizes),
+            bestseller: bestseller === 'true' ? true : false,
+            date: Date.now()
+        }
+
+        //price is passed as a json string so we have to convert it to number for db
+        //size also have to be parsed because it should be an array
+        //bestseller is passed as string instead of boolean so we converted it
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({success:true, message: "Product Added"});
 
     } catch (error) {
         console.log(error)
