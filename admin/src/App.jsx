9b8fa719113c46d79c86d7e6eabf1c11904a4727
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-18 17:05:23 +0530

    created components and pages and links for those pages
    created nabar and Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+        <Navbar></Navbar>
+        <hr></hr>
+
+        <div className='flex w-full'>
+          <Sidebar></Sidebar>
+          <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add></Add>}></Route>
+              <Route path='/list' element={<List></List>}></Route>
+              <Route path='/orders' element={<Orders></Orders>}></Route>
+            </Routes>
+
+          </div>
+
+        </div>
+      </>
+      
+      
+
+    </div>
   )
 }
 
