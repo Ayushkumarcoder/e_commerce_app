06382341ac205fba6b937161e9b8e0c1baf8cbee
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-18 15:17:23 +0530

    added the fuction to remove, get the list, and get info of single product in productController
    added the admin login and it's verification using middleware
    to make sure only admin can add and remove the product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -53,20 +53,44 @@ const addProduct = async (req, res) => {
 //function for listing product
 
 const listProduct = async (req, res) => {
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success: true, products});
 
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message});
+    }
 }
 
 // function for remove product 
 
 const removeProduct = async (req, res) => {
+    try {
 
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Product Removed"});
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message});
+    }
 }
 
 
 // function for single product info
 
 const singleProduct = async (req, res) => {
+    try {
 
+        const product = await productModel.findById(req.body.id);
+        res.json({success: true, product});
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message});
+    }
 }
 
 export {addProduct, listProduct, removeProduct, singleProduct}
