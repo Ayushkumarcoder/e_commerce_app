3bb7ff881e81d06ed9099e90785e37cc09276158
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-19 18:24:16 +0530

    completed the admin login section now admin can successfully login and the token is saved in local storage logout button is also working

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import {Routes, Route} from 'react-router-dom'
-import Add from './pages/Add'
-import List from './pages/List'
-import Orders from './pages/Orders'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { ToastContainer } from 'react-toastify';
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'): "");
+
+  useEffect(()=>{
+    localStorage.setItem('token', token);
+  },[token])
+
   return (
-    <div className='bg-gray-50 min-h-screen'>
-      <>
-        <Navbar></Navbar>
-        <hr></hr>
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken}></Login>
+      ) : (
+        <>
+          <Navbar setToken={setToken}></Navbar>
+          <hr></hr>
 
-        <div className='flex w-full'>
-          <Sidebar></Sidebar>
-          <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
-            <Routes>
-              <Route path='/add' element={<Add></Add>}></Route>
-              <Route path='/list' element={<List></List>}></Route>
-              <Route path='/orders' element={<Orders></Orders>}></Route>
-            </Routes>
-
+          <div className="flex w-full">
+            <Sidebar></Sidebar>
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token}></Add>}></Route>
+                <Route path="/list" element={<List token={token}></List>}></Route>
+                <Route path="/orders" element={<Orders token={token}></Orders>}></Route>
+              </Routes>
+            </div>
           </div>
-
-        </div>
-      </>
-      
-      
-
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
