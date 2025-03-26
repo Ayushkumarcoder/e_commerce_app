7ca486353f380bf9c60f7d89428e2479a1cc419d
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-26 15:47:30 +0530

    completed my orders panel for user now user can see all their orders in myorder secton

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -56,6 +56,21 @@ const allOrders = async(req, res)=>{
 
 const userOrders = async(req, res)=>{
 
+    try {
+
+        const {userId} = req.body;
+
+        const orders = await orderModel.find({userId});
+        res.json({success: true, orders});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+        
+    }
+
+
+
 }
 
 //update order status from the admin panel
